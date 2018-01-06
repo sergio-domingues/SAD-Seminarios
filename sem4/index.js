568726efc459ba6a152483807dc537af2f09b155
sergio <sergio.mieic@gmail.com>
2018-01-07 00:11:29 +0100

    act 1 e 2

--- sem4/index.js
+++ sem4/index.js
@@ -2,21 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.broadcast.emit('connectionMsg', "a user connected");
+
+  socket.on('disconnect', function () {
+    console.log('a user disconnected');
+    io.emit('disconnectionMsg', "a user disconnected");
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+
+  socket.on('chat message', function (msg) {    
+    socket.broadcast.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
